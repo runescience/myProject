c7115e54461bef04c42fb1d805e6bcafc31f69df
joel siragher <joel.siragher@gmail.com>
2023-05-30 20:08:33 -0400

    some message

---
+++ resources/views/hazard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+<h1> private property (c) 2023 jsiragher</h1>
+
+@section('content')
+
+    <h1>hazard Page {{$id}} {{$name}} {{$password}}</h1>
+@stop
+
+
+
---
+++ resources/views/universe.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+<h1> private property (c) 2023 jsiragher</h1>
+
+@section('content')
+
+    <h1>Universe page {{$id}} {{$name}} {{$password}}</h1>
+@stop
+
+
+
